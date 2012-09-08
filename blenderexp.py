f5edb0443c25a8e2f34ecd232e006b87b6030577
Ingo Ruhnke <grumbel@gmail.com>
2012-09-09 01:48:19 +0200

    Added copyright header

--- blenderexp.py
+++ blenderexp.py
@@ -1,10 +1,26 @@
+##  Blender Export Script
+##  Copyright (C) 2012 Ingo Ruhnke <grumbel@gmail.com>
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import bpy
 
-def writeObject(object):
-        if object.type == 'MESH':
-                mesh = object.data
-                writeVertices(mesh.vertices, object.location)
-                writeFaces(mesh.faces)
+def writeObject(obj):
+        if obj.type == 'MESH':
+                mesh = obj.data
+        writeVertices(mesh.vertices, obj.location)
+        writeFaces(mesh.faces)
 
 def writeVertices(vertices, location):
         outfile.write("%d\n" % len(vertices))
@@ -19,19 +35,32 @@ def writeVertices(vertices, location):
                                  i.co.y + location.y,
                                  i.co.z + location.z))
 
-def writeFaces(faces):
-        outfile.write("%d\n" % (len(faces)))
-        for i in faces:
-                outfile.write("%d %d %d\n" % (i.vertices[0], i.vertices[1], i.vertices[2]))
+def writeFaces(faces_data):       
+        # collect data from Blender and triangulate it
+        faces = []
+        for i in faces_data:
+                if len(i.vertices) == 3:
+                        faces.append((i.vertices[0], i.vertices[1], i.vertices[2]))
+                elif len(i.vertices) == 4:
+                        faces.append((i.vertices[0], i.vertices[1], i.vertices[2]))
+                        faces.append((i.vertices[0], i.vertices[3], i.vertices[2]))
+                else: 
+                        print("unhandled number of faces: %d" % len(i.vertices))
+
+        # write data to file
+        outfile.write("%d\n" % len(faces))
+        for face in faces:
+                outfile.write("%d %d %d\n" % face)
 
 
 outfile = open("/tmp/blender.mod", "w")
 
 outfile.write("%d\n" % (len(bpy.data.objects)))
-for child in bpy.data.objects:
-    writeObject(child)
+objects = [obj for obj in bpy.data.objects if obj.type == 'MESH']
+for child in objects:
+        writeObject(child)
         
 outfile.close()
-
+print("-- export complete --")
 
 # EOF #
--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,3 +1,19 @@
+//  Simple 3D Model Viewer
+//  Copyright (C) 2012 Ingo Ruhnke <grumbel@gmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "mesh.hpp"
 #include "log.hpp"
 
@@ -106,55 +122,27 @@ Mesh::draw()
     glDisable(GL_LIGHTING);
     glColor3f(0, 1.0f, 1.0f);
     glBegin(GL_LINES);
-    for (FaceLst::iterator i = faces.begin(); i != faces.end(); ++i)
+    for (FaceLst::iterator face = faces.begin(); face != faces.end(); ++face)
     {
-      draw_face_normal(*i);
+      float s = 0.1f;
+      glVertex3f(vertices[face->vertex1].x, vertices[face->vertex1].y, vertices[face->vertex1].z);
+      glVertex3f(vertices[face->vertex1].x + normals[face->vertex1].x * s,
+                 vertices[face->vertex1].y + normals[face->vertex1].y * s,
+                 vertices[face->vertex1].z + normals[face->vertex1].z * s);
+
+      glVertex3f(vertices[face->vertex2].x, vertices[face->vertex2].y, vertices[face->vertex2].z);
+      glVertex3f(vertices[face->vertex2].x + normals[face->vertex2].x * s,
+                 vertices[face->vertex2].y + normals[face->vertex2].y * s,
+                 vertices[face->vertex2].z + normals[face->vertex2].z * s);
+
+      glVertex3f(vertices[face->vertex3].x, vertices[face->vertex3].y, vertices[face->vertex3].z);
+      glVertex3f(vertices[face->vertex3].x + normals[face->vertex3].x * s,
+                 vertices[face->vertex3].y + normals[face->vertex3].y * s,
+                 vertices[face->vertex3].z + normals[face->vertex3].z * s);
     }
     glEnd();
     glEnable(GL_LIGHTING);
   }
-}
-
-void
-Mesh::draw_face_normal(const Face& face)
-{
-  Vector normal = calc_face_normal(face);
-    
-  Vector pos(vertices[face.vertex1]
-             + vertices[face.vertex2]
-             + vertices[face.vertex3]);
-  pos = pos * (1.0f/3.0f);
-
-  glVertex3f(pos.x, pos.y, pos.z);
-  pos = pos +(0.5* normal);
-  glVertex3f(pos.x, pos.y, pos.z);
-}
-
-Vector
-Mesh::calc_face_normal(const Face& face)
-{
-  Vector u(vertices[face.vertex2].x - vertices[face.vertex1].x,
-           vertices[face.vertex2].y - vertices[face.vertex1].y,
-           vertices[face.vertex2].z - vertices[face.vertex1].z);
-
-  Vector v(vertices[face.vertex3].x - vertices[face.vertex2].x,
-           vertices[face.vertex3].y - vertices[face.vertex2].y,
-           vertices[face.vertex3].z - vertices[face.vertex2].z);
-
-  Vector normal(u.y*v.z - u.z*v.y,
-                u.z*v.x - u.x*v.z,
-                u.x*v.y - u.y*v.x);
-
-  normal.normalize();
-
-  return normal;
-}
-
-void
-Mesh::set_face_normal(const Face& face)
-{
-  const Vector& normal = calc_face_normal(face);
-  glNormal3f(normal.x, normal.y, normal.y);
 }
 
 /* EOF */
