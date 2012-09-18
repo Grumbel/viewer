71206026e4cd781baa42ba5450965d40eff4472b
Ingo Ruhnke <grumbel@gmail.com>
2012-09-18 18:59:35 +0200

    Switched to float framebuffer

--- src/framebuffer.hpp
+++ src/framebuffer.hpp
@@ -40,7 +40,7 @@ public:
     // create color buffer texture
     glGenTextures(1, &m_color_buffer);
     glBindTexture(GL_TEXTURE_2D, m_color_buffer);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,  width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB16F,  width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
