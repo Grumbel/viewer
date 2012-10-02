a17c65b61314924a9abd74c6bf12405edb577d6a
Ingo Ruhnke <grumbel@gmail.com>
2012-10-02 13:02:20 +0200

    Added exporting of bone/vertex-group weight

--- blenderexp.py
+++ blenderexp.py
@@ -15,24 +15,50 @@
 ##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import bpy
+from mathutils import Matrix, Vector
 from collections import namedtuple
 
 Face   = namedtuple('Face',   ['v1', 'v2', 'v3'])
-Vertex = namedtuple('Vertex', ['co', 'n', 'uv'])
+Vertex = namedtuple('Vertex', ['co', 'n', 'uv', 'bones'])
 
-def writeObj(objects):
-    faces = []
-    for obj in objects:
-        faces += collect_faces(obj.data, obj.location)
+# "B2OGL * vec" results in OpenGL coordinates
+B2OGL = Matrix(((1, 0,  0, 0),
+                (0, 0, -1, 0),
+                (0, 1,  0, 0),
+                (0, 0,  0, 1)))
 
+def vec3(v):
+    """Convert Blender vector into tuple, swaps Y and Z"""
+    # return (v.x, v.z, -v.y)
+    return (v.x, v.y, v.z)
+
+def writeObj(obj):
+    faces = collect_faces(obj.data)
     faces, vertices = index_vertices(faces)
 
-    # outfile.write("g %s\n" % obj.name)
+    outfile.write("o %s\n" % obj.name)
+    outfile.write("loc %f %f %f\n" % vec3(obj.location))
 
     for v in vertices:
         outfile.write("vn %f %f %f\n" % v.n)
         if v.uv:
             outfile.write("vt %f %f\n" % v.uv)
+
+        bones = list(v.bones)
+        while len(bones) < 4:
+            bones.append((0, 0.0))
+
+        # FIXME: should only discard least important bone
+        while len(bones) > 4:
+            bones.pop()
+
+        bone_index  = [g for g, w in bones]
+        bone_weight = [w for g, w in bones]
+        bone_weight = [w / sum(bone_weight) for w in bone_weight]
+
+        outfile.write("bi %d %d %d %d\n" % tuple(bone_index))
+        outfile.write("bw %f %f %f %f\n" % tuple(bone_weight))
+        
         outfile.write("v %f %f %f\n" % v.co)
 
     for f in faces:
@@ -61,11 +87,7 @@ def index_vertices(faces):
 
     return out_faces, list(vertices.keys())
 
-def to_tuple(vec):
-    """Convert Blender vector into tuple, swaps Y and Z"""
-    return (vec.x, vec.z, -vec.y)
-
-def collect_faces(mesh, loc):
+def collect_faces(mesh):
     """collect data from the given mesh and triangulate it"""
 
     uv_faces = None
@@ -74,41 +96,44 @@ def collect_faces(mesh, loc):
         uv_faces = mesh.uv_textures.active.data
 
     out_faces = []
+
+    # bpy.data.objects[0].data.vertices[1].groups -> VertexGroupElement
+    # bpy.data.objects[0].data.vertices[1].groups[0] -> weight
         
-    for i in range(0, len(faces)):
-        v1 = mesh.vertices[faces[i].vertices[0]]
-        v2 = mesh.vertices[faces[i].vertices[1]]
-        v3 = mesh.vertices[faces[i].vertices[2]]
-        if len(faces[i].vertices) == 4:
-            v4 = mesh.vertices[faces[i].vertices[3]]
+    for face in faces:
+        num_vertices = len(face.vertices)
+        v = [mesh.vertices[face.vertices[i]] for i in range(0, num_vertices)]
 
-        uv1 = uv2 = uv3 = uv4 = None
         if uv_faces:
-            uv1 = uv_faces[i].uv1.to_tuple()
-            uv2 = uv_faces[i].uv2.to_tuple()
-            uv3 = uv_faces[i].uv3.to_tuple()
-            if len(faces[i].vertices) == 4:
-                uv4 = uv_faces[i].uv4.to_tuple()
+            uv = [tuple(uv_faces[face.index].uv[i]) for i in range(0, num_vertices)]
+        else:
+            uv = [None, None, None, None]
 
-        face = Face(Vertex(to_tuple(v1.co + loc), to_tuple(v1.normal), uv1),
-                    Vertex(to_tuple(v2.co + loc), to_tuple(v2.normal), uv2),
-                    Vertex(to_tuple(v3.co + loc), to_tuple(v3.normal), uv3))
+        bones = [[], [], [], []]
+        for i, vert in enumerate(v):
+            for j, g in enumerate(mesh.vertices[vert.index].groups):
+                bones[i].append((g.group, g.weight))
+            bones[i] = tuple(bones[i])
+        bones = tuple(bones)
 
-        out_faces.append(face)
+        out_faces.append(
+            Face(Vertex(vec3(v[0].co), vec3(v[0].normal), uv[0], bones[0]),
+                 Vertex(vec3(v[1].co), vec3(v[1].normal), uv[1], bones[1]),
+                 Vertex(vec3(v[2].co), vec3(v[2].normal), uv[2], bones[2])))
        
-        if len(faces[i].vertices) == 4:
-            face = Face(Vertex(to_tuple(v1.co + loc), to_tuple(v1.normal), uv1),
-                        Vertex(to_tuple(v3.co + loc), to_tuple(v3.normal), uv3),
-                        Vertex(to_tuple(v4.co + loc), to_tuple(v4.normal), uv4))
-
-            out_faces.append(face)
+        if num_vertices == 4:
+            out_faces.append(
+                Face(Vertex(vec3(v[0].co), vec3(v[0].normal), uv[0], bones[0]),
+                     Vertex(vec3(v[2].co), vec3(v[2].normal), uv[2], bones[2]),
+                     Vertex(vec3(v[3].co), vec3(v[3].normal), uv[3], bones[3])))
 
     return out_faces
 
 with open("/tmp/blender.mod", "w") as outfile:
     outfile.write("# exported by %s\n" % __file__)
     objects = [obj for obj in bpy.data.objects if obj.type == 'MESH']
-    writeObj(objects)
+    for obj in objects:
+        writeObj(obj)
     outfile.write("\n# EOF #\n")
 
 print("-- export complete --")
