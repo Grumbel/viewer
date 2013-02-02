7a9c4be2185e27d73727808e18bdf53b1c17843c
Ingo Ruhnke <grumbel@gmail.com>
2013-02-02 03:58:42 +0100

    Added some copyright headers

--- src/scene_node.cpp
+++ src/scene_node.cpp
@@ -1,4 +1,77 @@
+//  Simple 3D Model Viewer
+//  Copyright (C) 2012-2013 Ingo Ruhnke <grumbel@gmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "scene_node.hpp"
+
+SceneNode::SceneNode(const std::string& name) :
+  m_name(name),
+  m_position(0.0f, 0.0f, 0.0f),
+  m_orientation(1.0f, 0.0f, 0.0f, 0.0f),
+  m_scale(1.0f , 1.0f, 1.0f),
+  m_global_transform(1),
+  m_children(),
+  m_entities()
+{
+}
+
+SceneNode::~SceneNode()
+{
+}
+
+void
+SceneNode::set_position(const glm::vec3& p) 
+{
+  m_position = p; 
+}
+
+glm::vec3
+SceneNode::get_position() const 
+{
+ return m_position; 
+}
+
+void
+SceneNode::set_orientation(const glm::quat& q) 
+{
+ m_orientation = q; 
+}
+
+glm::quat
+SceneNode::get_orientation() const 
+{
+ return m_orientation; 
+}
+
+void
+SceneNode::set_scale(const glm::vec3& s) 
+{
+ m_scale = s; 
+}
+
+glm::vec3
+SceneNode::get_scale() const 
+{
+ return m_scale; 
+}
+
+glm::mat4
+SceneNode::get_transform() const 
+{
+ return m_global_transform; 
+}
 
 void
 SceneNode::update_transform(const glm::mat4& parent_transform)
@@ -13,6 +86,27 @@ SceneNode::update_transform(const glm::mat4& parent_transform)
   {
     child->update_transform(m_global_transform);
   }
+}
+
+void
+SceneNode::attach_entity(ModelPtr entity)
+{
+  m_entities.push_back(entity);
+}
+
+void
+SceneNode::attach_child(std::unique_ptr<SceneNode> child)
+{
+  m_children.push_back(std::move(child));
+}
+
+SceneNode*
+SceneNode::create_child() 
+{
+  std::unique_ptr<SceneNode> child(new SceneNode); 
+  SceneNode* ptr = child.get();
+  attach_child(std::move(child));
+  return ptr;
 }
 
 /* EOF */
--- src/scene_node.hpp
+++ src/scene_node.hpp
@@ -1,3 +1,19 @@
+//  Simple 3D Model Viewer
+//  Copyright (C) 2012-2013 Ingo Ruhnke <grumbel@gmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef HEADER_SCENE_NODE_HPP
 #define HEADER_SCENE_NODE_HPP
 
@@ -21,50 +37,25 @@ private:
   std::vector<ModelPtr> m_entities;
 
 public:
-  SceneNode(const std::string& name = std::string()) :
-    m_name(name),
-    m_position(0.0f, 0.0f, 0.0f),
-    m_orientation(1.0f, 0.0f, 0.0f, 0.0f),
-    m_scale(1.0f , 1.0f, 1.0f),
-    m_global_transform(1),
-    m_children(),
-    m_entities()
-  {}
+  SceneNode(const std::string& name = std::string());
+  ~SceneNode(); 
 
-  ~SceneNode()
-  {
-  }
+  void set_position(const glm::vec3& p);
+  glm::vec3 get_position() const;
 
-  void set_position(const glm::vec3& p) { m_position = p; }
-  glm::vec3 get_position() const { return m_position; }
+  void set_orientation(const glm::quat& q);
+  glm::quat get_orientation() const;
 
-  void set_orientation(const glm::quat& q) { m_orientation = q; }
-  glm::quat get_orientation() const { return m_orientation; }
+  void set_scale(const glm::vec3& s);
+  glm::vec3 get_scale() const;
 
-  void set_scale(const glm::vec3& s) { m_scale = s; }
-  glm::vec3 get_scale() const { return m_scale; }
-
-  glm::mat4 get_transform() const { return m_global_transform; }
+  glm::mat4 get_transform() const;
 
   void update_transform(const glm::mat4& parent_transform = glm::mat4(1));
 
-  void attach_entity(ModelPtr entity)
-  {
-    m_entities.push_back(entity);
-  }
-
-  void attach_child(std::unique_ptr<SceneNode> child)
-  {
-    m_children.push_back(std::move(child));
-  }
-
-  SceneNode* create_child() 
-  {
-    std::unique_ptr<SceneNode> child(new SceneNode); 
-    SceneNode* ptr = child.get();
-    attach_child(std::move(child));
-    return ptr;
-  }
+  void attach_entity(ModelPtr entity);
+  void attach_child(std::unique_ptr<SceneNode> child);
+  SceneNode* create_child();
 
   const std::vector<std::unique_ptr<SceneNode> >& get_children() const { return m_children; }
   const std::vector<ModelPtr>&   get_entities() const { return m_entities; }
